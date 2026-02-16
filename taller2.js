1e73bbff0316119795085ecbb2d0432df0815800
paolapechene <paolapechenemartinez@gmail.com>
2026-02-16 13:46:05 -0500

    taller2.js

---
+++ taller2.js
@@ -0,0 +1,20 @@
+/*  ¿Qué tipo de dato devuelve prompt()?
+rta:
+el tipo dato que devuelve prompt es un string(cadena de texto)
+ */
+
+/* ¿Por qué es necesario usar Number() o ParseInt Investigue si es necesario?
+rta:
+es necesario usarlos porque prompt devuelve el texto y para realizar
+operaciones matematicas correctamente necesitamos convertir ese texto en dato numerico.
+*/
+
+/*¿Qué pasa si sumamos directamente dos valores de prompt() sin convertirlos? 
+Ejemplo: java los une como texto, no los suma como numeros
+*/
+/* "5" + "3" el resultado sera "53",en cambio si convertimos 5 + 3 
+el resultado sera de 8*/
+
+/*  ¿Qué función bloquea la ejecución hasta que el usuario responda?
+las funciones son prompt(),y tambien estan alert() y confirm()
+*/
